3a60287d5d6e14e674448e532404c15448dd5429
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2017-09-26 17:21:34 +1000

    Added ramp data set

--- nempy.py
+++ nempy.py
@@ -13,12 +13,12 @@ def load_sample(day = datetime.datetime(2016,11,1)):
 				"AND SETTLEMENTDATE <= '2016-11-2'"
 	return dbi.data_framer("nemweb",sql,index_col=None)
 	
-offers = {	"coal":{"capacity":2000,"price":32,"color":'#323232'},
-			"coal2":{"capacity":2000,"price":35,"color":'#424242'},
-			"coal3":{"capacity":2000,"price":9,"color":'#662506'},
-			"gas1":{"capacity":2000,"price":40,"color":'#CC4C02'},
-			"gas2":{"capacity":2000,"price":45,"color":'#EC7014'},
-			"gas3":{"capacity":2000,"price":50,"color":'#FB9A29'}	}			
+offers = {	"coal":{"capacity":2000,"price":32,"color":'#323232',"ramp":50},
+			"coal2":{"capacity":2000,"price":35,"color":'#424242',"ramp":50},
+			"coal3":{"capacity":2000,"price":9,"color":'#662506',"ramp":50},
+			"gas1":{"capacity":2000,"price":40,"color":'#CC4C02',"ramp":200},
+			"gas2":{"capacity":2000,"price":45,"color":'#EC7014',"ramp":500},
+			"gas3":{"capacity":2000,"price":50,"color":'#FB9A29',"ramp":1000}	}			
 	
 def solve_dispatch(df):
 	for offer in offers:
